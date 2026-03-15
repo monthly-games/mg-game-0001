501145b1e0b4cc495d69e7c08741f4c17d6a73af
jungsu-lunark <jungsu.kim@lunark.ai>
2026-03-16 02:33:06 +0900

    fix(games): fix HasGameReference accessor gameRef→game

--- game/macos/Flutter/GeneratedPluginRegistrant.swift
+++ game/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,10 @@ import device_info_plus
 import firebase_analytics
 import firebase_core
 import firebase_crashlytics
+import firebase_messaging
 import firebase_remote_config
+import flutter_local_notifications
+import in_app_purchase_storekit
 import shared_preferences_foundation
 import webview_flutter_wkwebview
 
@@ -20,7 +23,10 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FLTFirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseRemoteConfigPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  InAppPurchasePlugin.register(with: registry.registrar(forPlugin: "InAppPurchasePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
